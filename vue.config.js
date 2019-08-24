74a6fe2ebdd8f2ca9b7f89106aed5baa583dded9
954545647 <954545647@qq.com>
2019-08-25 02:45:40 +0800

    修改config.js文件优化首屏

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,33 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
   // publicPath: '/',
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+  // 取消 sourceMap
+  productionSourceMap: process.env.NODE_ENV === "production" ? false : true,
+  chainWebpack: config => {
+    // 移除 prefetch 插件
+    config.plugins.delete("prefetch");
+    // 移除 preload 插件
+    // config.plugins.delete("preload");
+    // 压缩代码
+    config.optimization.minimize(true);
+    // 分割代码，可以并行下载，不过也需要权衡数目
+    config.optimization.splitChunks({
+      chunks: "all"
+    });
+    // 配置分析插件
+    if (process.env.NODE_ENV === "production") {
+      if (process.env.npm_config_report) {
+        config
+          .plugin("webpack-bundle-analyzer")
+          .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin)
+          .end();
+        config.plugins.delete("prefetch");
+      }
+    }
+  },
+  // CSS 配置
+  css: {
+    // 是否动态的将CSS代码生成style标签插入到js文件中
+    extract: true // 否
+  },
 };
